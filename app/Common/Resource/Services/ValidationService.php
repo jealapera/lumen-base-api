292b83814a9f9724a0d45457018998ff011c9356
Jemari <jealapera@gmail.com>
2018-07-23 17:29:40 +0800

    Additional Service for Custom Validation

---
+++ app/Common/Resource/Services/ValidationService.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace App\Common\Resource\Services;
+
+/**
+ * Class for Custom Validation
+ * Class ValidationService
+ */
+class ValidationService
+{
+    // Custom Validation goes here...
+}
