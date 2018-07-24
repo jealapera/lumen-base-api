588a30413ead9439f9a1f7b22483aecd65724358
Jemari <jealapera@gmail.com>
2018-07-24 15:47:51 +0800

    Added Models Folder

---
+++ app/Models/SampleModel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SampleModel extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        //
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        //
+    ];
+
+    /**
+     * The rules for the validation of the given data.
+     *
+     * @var array
+     */
+    public $rules = [
+        //
+    ];
+}
